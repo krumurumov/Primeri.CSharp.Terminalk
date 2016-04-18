8c3ec11fff3332a59c18437808fbcfd95292a159
Крум Урумов <surumovl@abvl.bg>
2016-04-18 17:53:46 +0300

    Модул 02, Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефинирани на променливи
+
+
+			//Събиране на числа
+
+
+			//Писане в конзола
+
+		}
+	}
+}
