83150c104853f2d55d4d7bb7900fa4b8ffd82f70
Крум Урумов <surumovl@abvl.bg>
2016-04-26 15:16:39 +0300

    Модул 02, Видео 07 - Работа с масиви

---
+++ Modul02/TableProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TableProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[] row = new string[3];
+			row [0] = "Ред 1";
+			row [1] = "Ред 2";
+			row [2] = "Ред 3";
+
+			Console.WriteLine ("Масив row: " + row[0] + "," +row[1] +"," + row[2] +"\n");
+
+			string[] parce = "1,2,3,4,5,6,7,8,9".Split (',');
+			Console.WriteLine ("Броят на масива е\n: " + "1,2,3,4,5,6,7,8,9\n\n e:" +parce.Length);
+
+			string list1 = string.Join (";", parce);
+			Console.WriteLine ("Новият стринг е: \n" + list1 + "\n\n");
+		}
+	}
+}
